1b19fe942b54cbed18ce5fe3fbab05988dcb5287
luch2001lkop@gmail.com <luch2001lkop@gmail.com>
2026-06-09 19:07:14 +0300

    final

---
+++ BugPro/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using Stateless;
+
+namespace BugPro
+{
+    public enum State
+    {
+        NewDefect,
+        Triage,
+        Fixing,
+        Testing,
+        Closed,
+        Reopened,
+        Deferred,
+        CannotReproduce,
+        Returned
+    }
+
+    public enum Trigger
+    {
+        Assign,
+        StartFix,
+        Duplicate,
+        NotABug,
+        WontFix,
+        CannotReproduce,
+        CannotReproduceOk,
+        CannotReproduceFail,
+        NoTimeNow,
+        NeedMoreInfo,
+        FixCompleted,
+        TestPassed,
+        TestFailed,
+        ProblemSolved,
+        Reopen,
+        
+    }
+
+    public class Bug
+    {
+        private readonly StateMachine<State, Trigger> _machine;
+
+        public State CurrentState => _machine.State;
+
+        public Bug()
+        {
+            _machine = new StateMachine<State, Trigger>(State.NewDefect);
+
+            _machine.Configure(State.NewDefect)
+                .Permit(Trigger.Assign, State.Triage);
+
+            _machine.Configure(State.Triage)
+                .Permit(Trigger.StartFix, State.Fixing)
+                .Permit(Trigger.Duplicate, State.Closed)
+                .Permit(Trigger.NotABug, State.Closed)
+                .Permit(Trigger.WontFix, State.Closed)
+                .Permit(Trigger.CannotReproduce, State.CannotReproduce);
+
+            _machine.Configure(State.Fixing)
+                .Permit(Trigger.FixCompleted, State.Testing)
+                .Permit(Trigger.NoTimeNow, State.Deferred)
+                .Permit(Trigger.NeedMoreInfo, State.Triage);
+
+            _machine.Configure(State.Testing)
+                .Permit(Trigger.TestPassed, State.Closed)
+                .Permit(Trigger.TestFailed, State.Fixing)
+                .Permit(Trigger.ProblemSolved, State.Reopened);
+
+            _machine.Configure(State.Closed)
+                .Permit(Trigger.Reopen, State.Reopened);
+
+            _machine.Configure(State.Reopened)
+                .Permit(Trigger.Assign, State.Triage);
+
+            _machine.Configure(State.Deferred)
+                .Permit(Trigger.Assign, State.Triage);
+
+            _machine.Configure(State.CannotReproduce)
+                .Permit(Trigger.CannotReproduceOk, State.Closed)
+                .Permit(Trigger.CannotReproduceFail, State.Returned);
+
+            _machine.Configure(State.Returned)
+                .Permit(Trigger.Assign, State.Triage);
+        }
+
+        public void Fire(Trigger trigger)
+        {
+            _machine.Fire(trigger);
+        }
+
+        public bool CanFire(Trigger trigger)
+        {
+            return _machine.CanFire(trigger);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bug = new Bug();
+            Console.WriteLine($"State: {bug.CurrentState}");
+
+            bug.Fire(Trigger.Assign);
+            Console.WriteLine($"State: {bug.CurrentState}");
+
+            bug.Fire(Trigger.StartFix);
+            Console.WriteLine($"State: {bug.CurrentState}");
+
+            bug.Fire(Trigger.FixCompleted);
+            Console.WriteLine($"State: {bug.CurrentState}");
+
+            bug.Fire(Trigger.TestPassed);
+            Console.WriteLine($"State: {bug.CurrentState}");
+        }
+    }
+}
---
+++ BugTest/Test1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BugPro;
+using Stateless;
+
+namespace BugTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void InitialState_IsNewDefect()
+        {
+            var bug = new Bug("Test Bug");
+            Assert.AreEqual(State.NewDefect, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Assign_FromNewDefect_GoesToTriage()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartFix_FromTriage_GoesToFixing()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            Assert.AreEqual(State.Fixing, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NotADefect_FromTriage_GoesToNotABug()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.NotADefect);
+            Assert.AreEqual(State.NotABug, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void WontFix_FromTriage_GoesToClosed()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.WontFix);
+            Assert.AreEqual(State.Closed, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Duplicate_FromTriage_GoesToDuplicate()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.Duplicate);
+            Assert.AreEqual(State.Duplicate, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Defer_FromTriage_GoesToDeferred()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.Defer);
+            Assert.AreEqual(State.Deferred, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void FixCompleted_FromFixing_GoesToTesting()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.FixCompleted);
+            Assert.AreEqual(State.Testing, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void TestPassed_FromTesting_GoesToClosed()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.FixCompleted);
+            bug.Fire(Trigger.TestPassed);
+            Assert.AreEqual(State.Closed, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void TestFailed_FromTesting_GoesToFixing()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.FixCompleted);
+            bug.Fire(Trigger.TestFailed);
+            Assert.AreEqual(State.Fixing, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Reopen_FromClosed_GoesToReopened()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.FixCompleted);
+            bug.Fire(Trigger.TestPassed);
+            bug.Fire(Trigger.Reopen);
+            Assert.AreEqual(State.Reopened, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void StartFix_FromReopened_GoesToFixing()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.FixCompleted);
+            bug.Fire(Trigger.TestPassed);
+            bug.Fire(Trigger.Reopen);
+            bug.Fire(Trigger.StartFix);
+            Assert.AreEqual(State.Fixing, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NoTimeNow_FromFixing_GoesToDeferred()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.NoTimeNow);
+            Assert.AreEqual(State.Deferred, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NeedsSeparateSolution_FromFixing_GoesToTriage()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.NeedsSeparateSolution);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void OtherProductProblem_FromFixing_GoesToTriage()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.OtherProductProblem);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void NeedMoreInfo_FromFixing_GoesToTriage()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.NeedMoreInfo);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void CannotReproduce_FromTriage_GoesToCannotReproduce()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.CannotReproduce);
+            Assert.AreEqual(State.CannotReproduce, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void CannotReproduceOK_FromCannotReproduce_GoesToClosed()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.CannotReproduce);
+            bug.Fire(Trigger.CannotReproduceOK);
+            Assert.AreEqual(State.Closed, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void CannotReproduceNotOK_FromCannotReproduce_GoesToReturned()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.CannotReproduce);
+            bug.Fire(Trigger.CannotReproduceNotOK);
+            Assert.AreEqual(State.Returned, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void Assign_FromDeferred_GoesToTriage()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.Defer);
+            bug.Fire(Trigger.Assign);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_StartFix_FromNewDefect_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.StartFix);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_FixCompleted_FromNewDefect_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.FixCompleted);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_TestPassed_FromNewDefect_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.TestPassed);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_Reopen_FromNewDefect_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Reopen);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_Assign_FromTriage_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.Assign);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_StartFix_FromFixing_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.StartFix);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_FixCompleted_FromClosed_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.WontFix);
+            bug.Fire(Trigger.FixCompleted);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void Invalid_TestFailed_FromNewDefect_ThrowsException()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.TestFailed);
+        }
+
+        [TestMethod]
+        public void BugProperties_AreSetCorrectly()
+        {
+            var bug = new Bug("My Bug Title", "My Bug Description");
+            Assert.AreEqual("My Bug Title", bug.Title);
+            Assert.AreEqual("My Bug Description", bug.Description);
+        }
+
+        [TestMethod]
+        public void CanFire_ReturnsTrueForValidTransition()
+        {
+            var bug = new Bug("Test Bug");
+            Assert.IsTrue(bug.CanFire(Trigger.Assign));
+        }
+
+        [TestMethod]
+        public void CanFire_ReturnsFalseForInvalidTransition()
+        {
+            var bug = new Bug("Test Bug");
+            Assert.IsFalse(bug.CanFire(Trigger.StartFix));
+        }
+
+        [TestMethod]
+        public void CompleteWorkflow_NewToClosed()
+        {
+            var bug = new Bug("Test Bug");
+            Assert.AreEqual(State.NewDefect, bug.CurrentState);
+
+            bug.Fire(Trigger.Assign);
+            Assert.AreEqual(State.Triage, bug.CurrentState);
+
+            bug.Fire(Trigger.StartFix);
+            Assert.AreEqual(State.Fixing, bug.CurrentState);
+
+            bug.Fire(Trigger.FixCompleted);
+            Assert.AreEqual(State.Testing, bug.CurrentState);
+
+            bug.Fire(Trigger.TestPassed);
+            Assert.AreEqual(State.Closed, bug.CurrentState);
+        }
+
+        [TestMethod]
+        public void ReopenWorkflow_ClosedToReopenedToFixing()
+        {
+            var bug = new Bug("Test Bug");
+            bug.Fire(Trigger.Assign);
+            bug.Fire(Trigger.StartFix);
+            bug.Fire(Trigger.FixCompleted);
+            bug.Fire(Trigger.TestPassed);
+            bug.Fire(Trigger.Reopen);
+            bug.Fire(Trigger.StartFix);
+
+            Assert.AreEqual(State.Fixing, bug.CurrentState);
+        }
+    }
+}
